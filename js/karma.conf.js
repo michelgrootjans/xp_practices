5cf679b33680ca12ed38ca8b867841427a7bcb81
Michel Grootjans <michel.grootjans@gmail.com>
2016-12-12 15:10:30 +0100

    javascript: added gilded rose

--- js/karma.conf.js
+++ js/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
     frameworks: ['jasmine'],
     browsers: ['Chrome'],
     files: [
-      "lib/**/*.js",
+      "src/**/*.js",
       "spec/**/*.js"
     ],
 
---
+++ js/spec/first_test.js
@@ -0,0 +1,5 @@
+describe('my first test', function() {
+  it('succeedds', function() {
+    expect(1+1).toEqual(2);
+  });
+});
