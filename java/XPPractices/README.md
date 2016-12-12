3a605b2725b57579c1426811f26757851ff1c0bb
Michel Grootjans <michel.grootjans@gmail.com>
2016-12-12 12:29:01 +0100

    prepared java environment

---
+++ java/XPPractices/README.md
@@ -0,0 +1,5 @@
+# Java starting point
+prepared in Java version 1.8.0_112.
+
+## Instructions on running the test suite
+In your IDE : run your chosen test file, or run the entire suite.
---
+++ java/XPPractices/test/bowling/MyFirstTest.java
@@ -0,0 +1,19 @@
+package bowling;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyFirstTest {
+    @Before
+    public void setup()
+    {
+    }
+
+    @Test
+    public void shouldWork() {
+        assertEquals(0, 0);
+    }
+
+}
