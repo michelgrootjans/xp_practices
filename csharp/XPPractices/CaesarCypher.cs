1fd0cd7ec0528afdd0729362bb30ef94471cbfca
Michel Grootjans <michel.grootjans@gmail.com>
2016-12-14 15:52:51 +0100

    implemented caesar cypher

---
+++ csharp/XPPractices/CaesarCypher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace XPPractices
+{
+    public class CaesarCypher
+    {
+        private readonly int offset;
+        private readonly IList<string> alphabet;
+
+        public CaesarCypher(string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", int offset = 13)
+        {
+            this.offset = offset;
+            this.alphabet = alphabet.ToList();
+        }
+
+        public string Encrypt(string sentence)
+        {
+            return DoEncryption(sentence, offset);
+        }
+
+        public string Decrypt(string sentence)
+        {
+            return DoEncryption(sentence, -offset);
+        }
+
+        private string DoEncryption(string sentence, int offset)
+        {
+            return sentence.ToList()
+                .Select(letter => EncryptLetter(letter, offset))
+                .Aggregate("", (newSentence, letter) => newSentence + letter);
+        }
+
+        private string EncryptLetter(string letter, int offset)
+        {
+            var uppercase = letter.ToUpper();
+            if (!alphabet.Contains(uppercase)) return letter;
+
+            var letterPosition = alphabet.IndexOf(uppercase);
+            var newPosition = (letterPosition + alphabet.Count + offset) % alphabet.Count;
+            return alphabet[newPosition];
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static IList<string> ToList(this string letters)
+        {
+            return letters.Select(c => c.ToString()).ToList();
+        }
+    }
+}
--- csharp/XPPractices/CaesarCypherTests.cs
+++ csharp/XPPractices/CaesarCypherTests.cs
@@ -5,19 +5,57 @@ namespace XPPractices
     [TestFixture]
     public class CaesarCypherTests
     {
-        [Test]
-        public void EncryptA()
+        [TestCase("A", "N")]
+        [TestCase("AA", "NN")]
+        [TestCase("B", "O")]
+        [TestCase("N", "A")]
+        [TestCase(";", ";")]
+        public void Encrypt(string original, string encrypted)
         {
             var cypher = new CaesarCypher();
-            Assert.AreEqual("N", cypher.Encrypt("A"));
+            Assert.AreEqual(encrypted, cypher.Encrypt(original));
+            Assert.AreEqual(original, cypher.Decrypt(cypher.Encrypt(original)));
         }
-    }
 
-    public class CaesarCypher
-    {
-        public string Encrypt(string sentence)
+        [TestCase("a", "N")]
+        public void AssymetricalEncrypt(string original, string encrypted)
         {
-            return "N";
+            var cypher = new CaesarCypher();
+            Assert.AreEqual(encrypted, cypher.Encrypt(original));
+            Assert.AreEqual(original.ToUpper(), cypher.Decrypt(cypher.Encrypt(original)));
+        }
+
+        [TestCase("C", "N")]
+        [TestCase("B", "O")]
+        [TestCase("A", "P")]
+        [TestCase("D", "Q")]
+        public void CustomAlphapbet(string original, string encrypted)
+        {
+            var cypher = new CaesarCypher(alphabet: "CBA" + "DEFGHIJKLMNOPQRSTUVWXYZ");
+            Assert.AreEqual(encrypted, cypher.Encrypt(original));
+            Assert.AreEqual(original, cypher.Decrypt(cypher.Encrypt(original)));
+        }
+
+        [TestCase("A", 1,  "B")]
+        [TestCase("A", 2,  "C")]
+        [TestCase("A", 25, "Z")]
+        [TestCase("A", 26, "A")]
+        public void CustomOffset(string original, int offset, string encrypted)
+        {
+            var cypher = new CaesarCypher(offset: offset);
+            Assert.AreEqual(encrypted, cypher.Encrypt(original));
+            Assert.AreEqual(original, cypher.Decrypt(cypher.Encrypt(original)));
+        }
+
+        [TestCase("A", "ABC", 1, "B")]
+        [TestCase("A", "ABC", 2, "C")]
+        [TestCase("A", "ABC", 3, "A")]
+        [TestCase("A", "CBA", 1, "C")]
+        public void CustomAlphabetAndOffset(string original, string alphabet, int offset, string encrypted)
+        {
+            var cypher = new CaesarCypher(alphabet: alphabet, offset: offset);
+            Assert.AreEqual(encrypted, cypher.Encrypt(original));
+            Assert.AreEqual(original, cypher.Decrypt(cypher.Encrypt(original)));
         }
     }
 }
